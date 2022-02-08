9a6604524c640db897a1ec078da6a9d5f028229b
devstackpro <devstackpro@gmail.com>
2022-02-08 06:44:01 -0300

    EXEMPLO DE TIPOS E COMENTÁRIOS

---
+++ carcaDados/pedi_020-Representantes copy.sql
@@ -0,0 +1,4 @@
+SELECT
+    NIVEL_ESTRUTURA, --type: VARCHAR2(1) | nullable: Y | Default: 0' | comments: | Pre Cadastro: | Obs: 
+    GRUPO_ESTRUTURA, --type: VARCHAR2(5) | nullable: Y | Default: 00000' | comments: | Pre Cadastro: | Obs: 
+ 
---
+++ carcaDados/pedi_020-Representantes.sql
@@ -0,0 +1,4 @@
+SELECT
+    COD_REP_CLIENTE, --type: NUMBER(5) | nullable: N | Default: 0 | comments: | Pre Cadastro: | Obs: Código do representante
+    TIPO_REPR, --type: NUMBER(1) | nullable: Y | Default: 1 | comments: (1) Micro, (2) Autônomo, (3) Jurídica, (4) Administrador, (5) Vendedor  | Pre Cadastro: | Obs: Tipo do representante conforme acima
+ 
