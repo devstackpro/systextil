4ff3d50a81fadeef93490988540d3ae8f2c124af
Igor <devstackpro@gmail.com>
2022-02-15 08:52:16 -0300

    tipagem tabela supr_055 - Parcela da condição

---
+++ carcaDados/pedi_070 - Capa Condição Pagto.sql
@@ -0,0 +1,20 @@
+SELECT
+    COND_PGT_CLIENTE --type: NUMBER(3) | nullable: N | Default: 0 | comments: | Pre Cadastro: | Obs: Codigo da condição pagto
+    DESCR_PG_CLIENTE --type: VARCHAR2(30) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
+    --type: NUMBER(3) | nullable: | Default: 0 | comments: | Pre Cadastro: | Obs:
