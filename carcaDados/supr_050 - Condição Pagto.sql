8d4206e71171c8c7b78013c9fbabfbd119a4802e
Igor <devstackpro@gmail.com>
2022-02-21 11:51:46 -0300

    DELETADA

--- carcaDados/supr_050 - Condição Pagto.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-   COND_PGTO_COMPRA, --type: NUMBER(3) | nullable: N | Default: 0 | comments: | Pre Cadastro: Obs: Código da condição de pagamento
-   DESCR_COND_PGTO, --type: VARCHAR2(20) | nullable: Y | Default: 0 | comments: | Pre Cadastro:  Obs: Descrição da condição de pagamento
-   AVISTA, --type: NUMBER(1) | nullable: Y | Default: 0 | comments: | Pre Cadastro:  Obs: Indica se a condição de pagamento é a vista ou a prazo: 0- A prazo 1- A vista
-
-
-
