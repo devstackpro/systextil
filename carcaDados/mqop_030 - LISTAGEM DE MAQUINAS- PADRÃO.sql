8edd0e3fc1fb152822df99d76bc19e84cc24e160
Igor <devstackpro@gmail.com>
2022-02-22 10:59:28 -0300

    ATUALIZACAO

--- carcaDados/mqop_030 - LISTAGEM DE MAQUINAS- PADRÃO.sql
+++ carcaDados/mqop_030 - LISTAGEM DE MAQUINAS- PADRÃO.sql
@@ -1,3 +1,5 @@
+-- VERSÃO 1 | CARGA DE DADOS
+
 SELECT
     MAQ_SUB_GRUPO_MQ, --type: VARCHAR2(4) | nullable:  | Default: '' | comments: | Pre Cadastro: Mqop_020 – subgrupos de maquinas | Obs: 
     MAQ_SUB_SBGR_MAQ, --type: VARCHAR2(3) | nullable:  | Default: '' | comments: | Pre Cadastro: | Obs: 
@@ -47,3 +49,142 @@ SELECT
     SUBGRU_MAQ_ANTERIOR, --type: VARCHAR2(3)  | nullable: Y | Default: '' | comments: | Pre Cadastro: | Obs:
     NUMERO_MAQ_ANTERIOR, --type: NUMBER(5)  | nullable: Y | Default: 0 | comments: | Pre Cadastro: | Obs:
 
+
+-- VERSÃO 1 | LISTAGEM PADRÃO 
+
+SELECT
+    MAQ_SUB_GRUPO_MQ, --text
+    MAQ_SUB_SBGR_MAQ, --text
+    NUMERO_MAQUINA, --integer
+    MAQUINA_ATIVA, --integer
+    NOME_MAQUINA, --text
+    QTDE_UPS, --float
+    NR_PATRIMONIO, --integer
+    CENTRO_CUSTO, --integer
+    RELACAO_BANHO, --float
+    VOLUME_BANHO, --integer
+    NUMERO_BOCAS, --integer
+    OBSERVACAO1, --text
+    OBSERVACAO2, --text
+    VOLUME_MAXIMO, --integer
+    VOLUME_MINIMO, --integer
+    CCUSTO_HOMEM, --integer
+    SERIE_COR, --integer
+    SUB_DIVISAO_TAB, --integer
+    FAMILIA_MAQ, --integer
+    SUB_DIVISAO_FAB, --integer
+    GRUPO_MAQ_ANTERIOR, --text
+    SUBGRU_MAQ_ANTERIOR, --text
+    NUMERO_MAQ_ANTERIOR, --integer
+    DESC_MODELO, --text
+    NR_FABRIC, --text
+    DT_FABRIC, --datetime
+    CGC9_FABRIC, --integer
+    CGC4_FABRIC, --integer
+    CGC2_FABRIC, --integer
+    TEMPO_MAX_TRAB, --integer
+    PRESSAO_AR, --integer
+    PRESSAO_VAPOR, --integer
+    PRESSAO_AGUA, --integer
+    PRESSAO_TRAB, --integer
+    TENSAO, --integer
+    POTENCIA_KVA, --integer
+    POTENCIA_KW, --integer
+    AMPER, --integer
+    UNIDADE_RELOGIO, --text
+    PASSO_RELOGIO, --float
+    ATUALIZA_ESTOQUE_FIO, --integer
+    MULTIPLICADOR_PRODUCAO_RELOGIO, --float
+    PESO_PADRAO_VOLUME, --float
+    TITULO_DA_FITA_DE_SAIDA, --float
+    MAXIMO_RELOGIO, --float
+    ULTIMA_LEITURA, --float
+    NR_PATRIMONIO_STR, --text
+    PRIORIDADE, --integer
+    APELIDO_MAQ, --text
+    ESTAGIO_PRINCIPAL, --integer
+    CODIGO_FORMULA, --integer
+    MAQUINA_INTEGRACAO, --text
+    IND_ENVIA_INFOTINT, --text
+    ALTURA_BANDEJA, --integer
+    NUMERO_DIVISOES_HORIZ, --integer
+    NUMERO_DIVISOES_VERT, --integer
+    ULTIMA_LEITURA2, --float
+    ULTIMA_LEITURA3, --float
+    ULTIMA_LEITURA4, --float
+    VOLUME_DECIMAL, --float
+    LETRA_AGRUP, --text
+    STATUS_AGULHEIRO, --integer
+    TIPO_PROCESSO, --text
+FROM 
+    mqop_030 
+
+
+-- VERSÃO 2 | LISTAGEM PADRÃO
+
+SELECT
+    MAQ_SUB_GRUPO_MQ, 
+    MAQ_SUB_SBGR_MAQ, 
+    NUMERO_MAQUINA,
+    MAQUINA_ATIVA,
+    NOME_MAQUINA, 
+    QTDE_UPS, 
+    NR_PATRIMONIO,
+    CENTRO_CUSTO,
+    RELACAO_BANHO, 
+    VOLUME_BANHO,
+    NUMERO_BOCAS,
+    OBSERVACAO1, 
+    OBSERVACAO2, 
+    VOLUME_MAXIMO,
+    VOLUME_MINIMO,
+    CCUSTO_HOMEM,
+    SERIE_COR,
+    SUB_DIVISAO_TAB,
+    FAMILIA_MAQ,
+    SUB_DIVISAO_FAB,
+    GRUPO_MAQ_ANTERIOR, 
+    SUBGRU_MAQ_ANTERIOR, 
+    NUMERO_MAQ_ANTERIOR,
+    DESC_MODELO, 
+    NR_FABRIC, 
+    DT_FABRIC, 
+    CGC9_FABRIC,
+    CGC4_FABRIC,
+    CGC2_FABRIC,
+    TEMPO_MAX_TRAB,
+    PRESSAO_AR,
+    PRESSAO_VAPOR,
+    PRESSAO_AGUA,
+    PRESSAO_TRAB,
+    TENSAO,
+    POTENCIA_KVA,
+    POTENCIA_KW,
+    AMPER,
+    UNIDADE_RELOGIO, 
+    PASSO_RELOGIO, 
+    ATUALIZA_ESTOQUE_FIO,
+    MULTIPLICADOR_PRODUCAO_RELOGIO, 
+    PESO_PADRAO_VOLUME, 
+    TITULO_DA_FITA_DE_SAIDA, 
+    MAXIMO_RELOGIO, 
+    ULTIMA_LEITURA, 
+    NR_PATRIMONIO_STR, 
+    PRIORIDADE,
+    APELIDO_MAQ, 
+    ESTAGIO_PRINCIPAL,
+    CODIGO_FORMULA,
+    MAQUINA_INTEGRACAO, 
+    IND_ENVIA_INFOTINT, 
+    ALTURA_BANDEJA,
+    NUMERO_DIVISOES_HORIZ,
+    NUMERO_DIVISOES_VERT,
+    ULTIMA_LEITURA2, 
+    ULTIMA_LEITURA3, 
+    ULTIMA_LEITURA4, 
+    VOLUME_DECIMAL, 
+    LETRA_AGRUP, 
+    STATUS_AGULHEIRO
+    TIPO_PROCESSO
+FROM 
+    mqop_030 
