6ed981405c29b59ab7c3c44b69e68c50fc89c6fa
Igor <devstackpro@gmail.com>
2022-02-22 12:25:22 -0300

    ATUALIZACAO

--- carcaDados/pedi_220 - Grupos Economicos.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    TIPO, --type: NUMBER(2) | nullable: N | Default: 0 | comments: | Pre Cadastro:  Obs: Indicador do tipo de Registro. Fixo 8
-    CODIGO, --type: NUMBER(5) | nullable: N | Default: 0 | comments: | Pre Cadastro: | Obs: Código do grupo economico
-    CODIGO_SALVO, --type: NUMBER(5) | nullable: N | Default: 0 | comments: | Pre Cadastro: | Obs: Código do grupo economico. Mesmo que o campo código
-    DESCRICAO, --type: VARCHAR2(30) | nullable: y | Default: '' | comments: | Pre Cadastro: | Obs: Nome do grupo economico
